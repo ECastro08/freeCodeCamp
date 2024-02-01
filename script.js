022db1c50adf1d91f1fdaa295c6dedf9f713b6d6
Ecastro08 <worldmechatronics2023@gmail.com>
2024-01-31 23:12:17 -0500

    siguiendo el reto de javascript dia 31/01/2024

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 let xp = 0;
 let health =100;
 let gold = 50;
@@ -5,6 +6,8 @@ let currentWeapon = 0;
 let fighting;
 let monsteHealth;
 let inventory = ['stick'];
+
+// seleccionando las etiquetas html desde javascript
 
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
@@ -16,6 +19,14 @@ const goldText = document.querySelector('#goldText');
 const monsterName = document.querySelector('#monsterName');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
+
 const locations = [
     {
       name: "town square",
@@ -28,6 +39,12 @@ const locations = [
       "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
       "button functions": [buyHealth, buyWeapon, goTown],
       text: "You enter the store."
+    },
+    {
+      name: "cave",
+      "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+      "button functions": [fightSlime, fightBeast, goTown],
+      text: "You enter the cave. You see some monsters."
     }
   ];
   
@@ -38,13 +55,13 @@ button3.onclick = fightDragon;
 
 
 function update(location) {
-    button1.innerText = "Go to store";
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
-    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
   }
   
   function goTown() {
@@ -52,11 +69,11 @@ function update(location) {
   }
   
   function goStore() {
-  
+    update(locations[1]);
   }
   
   function goCave() {
-    console.log("Going to cave.");
+    update(locations[2])
   }
   
   function fightDragon() {
@@ -64,9 +81,33 @@ function update(location) {
   }
   
   function buyHealth() {
-  
+    if (gold >= 10) {
+      gold -= 10;
+      health += 10;
+      goldText.innerText = gold;
+      healthText.innerText = health;
+    } else {
+      text.innerText = "You do not have enough gold to buy health.";
   }
   
   function buyWeapon() {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = `You now have a ${newWeapon}.`;
+      text.innerText += " In your inventory you have: " + inventory;
+      inventory.push(newWeapon);
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon."
+    }
+  }
+
+  function fightSlime() {
+
+  }
+  
+  function fightBeast() {
   
   }
